0513d5ce56dc1517c220035cb08c368507314a6b
q0abd <qiyomovabdulloh3@gmail.com>
2023-09-23 10:30:17 +0500

    stater tayyor

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,34 +1,34 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
-import About from "./../pages/About/About";
-import Error from "./../pages/Error/Error";
+import About from "../pages/About/About";
+import Error from "../pages/Error/Error";
 import Layout from "../pages/Layout/Layout.jsx";
 import Signup from "../pages/Auth/Signup.jsx";
 import Signin from "../pages/Auth/Signin.jsx";
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout />,
-        errorElement: < Error/>,
-        children: [
-            {
-                path: '/',
-                element: <Home/>
-            },
-            {
-                path: '/about',
-                element: <About/>
-            }
-        ]
-    },
-    {
-        path: '/sign-up',
-        element: <Signup/>
-    },
-    {
-        path: "/sign-in",
-        element: <Signin/>
-    }
-])
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+  {
+    path: "/sign-up",
+    element: <Signup />,
+  },
+  {
+    path: "/sign-in",
+    element: <Signin />,
+  },
+]);
 
-export  default  router;
+export default router;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,24 +1,7 @@
-import "./style.scss";
+import React from "react";
 
-const index = () => {
-  return (
-    <header>
-      <div className="container">
-        <div className="header__navbar">
-          <div className="header__logo">
-            <h1>COMPANY LOGO</h1>
-          </div>
-
-          <div className="header__center">
-            <button>
-              <i class="bx bx-menu"></i>
-              <span>Katalog</span>
-            </button>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+const Header = () => {
+  return <div>Header</div>;
 };
 
-export default index;
+export default Header;
